3d25d5caf0d3e086d63aa44ee0e4f853e451db10
Apit <apit@thezatapp.com>
2022-05-03 23:14:34 +0700

    adding splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:e_commerce_bloc/constant.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -13,8 +16,27 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () => Navigator.pushNamed(context, '/'));
     return Scaffold(
-      body: Column(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Image(
+              image: AssetImage('assets/images/logo.png'),
+              width: 125,
+              height: 125,
+            ),
+          ),
+          const SizedBox(
+            height: kSizeBox * 4,
+          ),
+          Text(
+            'Zero To Unicorn',
+            style: kTextStyle24BoldBlack,
+          )
+        ],
+      ),
     );
   }
 }
